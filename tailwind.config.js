a61fc30d01095c3bcd17a9adc7c560289f0ee1ca
Antony Merle <antony.merle@gmail.com>
2023-01-28 17:06:52 +0100

    logo & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: "#72ECCE",
+      },
+    },
   },
   plugins: [],
 };
