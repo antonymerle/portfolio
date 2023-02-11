449ac62447bd927780fa5331536ff947917e4008
Antony Merle <antony.merle@gmail.com>
2023-02-11 22:49:55 +0100

    project section fits content

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,6 +57,7 @@ export default function Home() {
           </div>
         </section>
 
+        {/* ABOUT */}
         <section id="about" className="w-full h-screen  bg-slate-50">
           <div
             id="about-container"
@@ -129,13 +130,14 @@ export default function Home() {
             </div>
           </div>
         </section>
+        {/* PROJETS */}
         <section
           id="projects"
-          className="h-screen w-full flex flex-col justify-start space-y-4  px-6  bg-slate-800 text-center text-slate-300  md:px-24 md:text-left lg:px-64"
+          className="h-max w-full flex flex-col flex-grow-1 justify-start space-y-4  px-6  bg-slate-800 text-center text-slate-300  md:px-24 md:text-left lg:px-64"
         >
           <div
             id="projects-container"
-            className="flex flex-col items-start h-screen w-full max-w-[900px]"
+            className="flex flex-col items-start h-full w-full max-w-[900px]"
           >
             <div
               id="projects-header"
@@ -149,8 +151,8 @@ export default function Home() {
             <ul className="space-y-44">
               {/* SHOWCASE PROJECT 1 */}
               {/* 1 ligne, 12 col. Image left 8 span 8 project card overflow 7/13 */}
-              <li className="relative w-full h-1/3 md:opacity-100 md:bg-none md:grid md:grid-cols-12 md:grid-rows-6 md:gap-2 md:overflow-hidden">
-                <div className="absolute transition-all hover:scale-105 hover:rounded-sm duration-300 top-0 left-0 w-full h-full opacity-40 bg-[url('../public/jellyfish.png')] bg-cover bg-no-repeat rounded-sm min-w-[200px] md:opacity-100 md:col-start-1 md:col-end-8 md:row-span-full md:bg-mint md:min-w-[600px] md:z-10 ">
+              <li className=" w-full h-[350px] md:opacity-100 md:bg-none md:grid md:grid-cols-12 md:grid-rows-6 md:gap-2 md:overflow-hidden">
+                <div className=" transition-all hover:scale-105 hover:rounded-sm duration-300 top-0 left-0 w-full h-full opacity-40 bg-[url('../public/jellyfish.png')] bg-cover bg-no-repeat rounded-sm min-w-[200px] md:opacity-100 md:col-start-1 md:col-end-8 md:row-span-full md:bg-mint md:min-w-[600px] md:z-10 ">
                   <div className="w-full h-full transition-all hover:bg-mint/0 rounded-sm duration-300  md:bg-mint/60 md:backdrop-brightness-75"></div>
                 </div>
                 <div className=" project-description md:col-start-7 md:col-end-13 md:row-span-full md:z-20">
@@ -188,8 +190,8 @@ export default function Home() {
                 </div>
               </li>
               {/* SHOWCASE PROJECT 2 */}
-              <li className="relative w-full h-1/3 md:opacity-100 md:bg-none md:grid md:grid-cols-12 md:grid-rows-6 md:gap-2 md:overflow-hidden">
-                <div className="absolute transition-all hover:scale-105 hover:rounded-sm duration-300 top-0 left-0 w-full h-full opacity-40 bg-[url('../public/jellyfish.png')] bg-cover bg-no-repeat rounded-sm min-w-[200px] md:opacity-100 md:col-start-5 md:col-end-13 md:row-span-full md:bg-mint md:min-w-[600px] md:z-10 ">
+              <li className=" w-full h-[350px] md:opacity-100 md:bg-none md:grid md:grid-cols-12 md:grid-rows-6 md:gap-2 md:overflow-hidden">
+                <div className=" transition-all hover:scale-105 hover:rounded-sm duration-300 top-0 left-0 w-full h-full opacity-40 bg-[url('../public/jellyfish.png')] bg-cover bg-no-repeat rounded-sm min-w-[200px] md:opacity-100 md:col-start-5 md:col-end-13 md:row-span-full md:bg-mint md:min-w-[600px] md:z-10 ">
                   <div className="w-full h-full transition-all hover:bg-mint/0 rounded-sm duration-300  md:bg-mint/60 md:backdrop-brightness-75"></div>
                 </div>
                 <div className=" project-description md:col-start-1 md:col-end-7 md:row-span-full md:z-20">
@@ -226,11 +228,51 @@ export default function Home() {
                   </ul>
                 </div>
               </li>
-              <li>project 3</li>
+              {/* SHOWCASE PROJECT 2 */}
+              <li className=" w-full h-[350px] md:opacity-100 md:bg-none md:grid md:grid-cols-12 md:grid-rows-6 md:gap-2 md:overflow-hidden">
+                <div className=" transition-all hover:scale-105 hover:rounded-sm duration-300 top-0 left-0 w-full h-full opacity-40 bg-[url('../public/jellyfish.png')] bg-cover bg-no-repeat rounded-sm min-w-[200px] md:opacity-100 md:col-start-1 md:col-end-8 md:row-span-full md:bg-mint md:min-w-[600px] md:z-10 ">
+                  <div className="w-full h-full transition-all hover:bg-mint/0 rounded-sm duration-300  md:bg-mint/60 md:backdrop-brightness-75"></div>
+                </div>
+                <div className=" project-description md:col-start-7 md:col-end-13 md:row-span-full md:z-20">
+                  <h3 className="py-8 text-slate-300 text-2xl font-bold md:col-start-9 md:col-end-13 md:row-span-1 md:text-right">
+                    JellyFish
+                  </h3>
+                  <p className="text-center bg-cybGrey rounded-sm text-slate-300 text-sm md:text-right p-4 md:col-start-7 md:col-end-13">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                    molestiae earum, provident corrupti iste dolorum maiores
+                    quisquam repellendus in similique dicta veritatis eius
+                    expedita labore incidunt laborum ex deserunt porro.
+                  </p>
+                  <ul className="project-technos flex justify-center text-sm space-x-2 py-4 text-mint md:justify-end">
+                    <li>MongoDB</li>
+                    <li>Express</li>
+                    <li>React</li>
+                    <li>NodeJS</li>
+                  </ul>
+                  <ul className="project-code-live flex justify-center  space-x-2 py-4 md:justify-end">
+                    <li>
+                      <FontAwesomeIcon
+                        icon={faGithubAlt}
+                        className="text-slate-300"
+                        style={{ height: "20px" }}
+                      />
+                    </li>
+                    <li>
+                      <FontAwesomeIcon
+                        icon={faRightFromBracket}
+                        style={{ height: "20px" }}
+                        className="text-slate-300"
+                      />
+                    </li>
+                  </ul>
+                </div>
+              </li>
             </ul>
           </div>
         </section>
-        <section id="contact">Let's talk.</section>
+        <section id="contact" className="w-full h-screen  bg-slate-50">
+          Let's talk.
+        </section>
       </main>
       <footer>Antony Merle, {new Date().getFullYear()}</footer>
     </div>
