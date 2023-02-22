8e246cf6081d6d587028f88697de9e9a64c24f41
Antony Merle <antony.merle@gmail.com>
2023-02-22 23:28:30 +0100

    footer content

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,22 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
-import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import {
+  faPlus,
+  faRightFromBracket,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithubAlt,
+  faLinkedinIn,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import jellyfish from "../public/jellyfish.png";
 import logo from "../public/logo.jpg";
+import logoFullSize from "../public/logo.png";
 import pp from "../public/antony-merle.jpg";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -326,8 +337,50 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className="flex justify-center items-center h-24 text-slate-300">
-        Antony Merle, {new Date().getFullYear()}
+
+      {/* FOOTER */}
+      <footer className="flex flex-col space-y-12 justify-center items-center h-[777px] text-slate-300">
+        <Image
+          src={logoFullSize}
+          alt="logoFullSize"
+          width={333}
+          height={333}
+          className="rounded-full "
+        />
+        <h6 className="text-2xl font- light">
+          Antony Merle, {new Date().getFullYear()}
+        </h6>
+
+        <ul className="flex flex-row space-x-10">
+          <a href="https://github.com/antonymerle" target="blank">
+            <li className="border-2 border-slate-100 p-6 rounded-full transition-colors duration-300 ease-in hover:bg-slate-600">
+              <FontAwesomeIcon icon={faGithubAlt} style={{ height: "33px" }} />
+            </li>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/antony-merle-25854042/"
+            target="blank"
+          >
+            <li className="border-2 border-slate-100 p-6 rounded-full transition-colors duration-300 ease-in hover:bg-slate-600">
+              <FontAwesomeIcon icon={faLinkedinIn} style={{ height: "33px" }} />
+            </li>
+          </a>{" "}
+          <a href="https://www.instagram.com/antonymerle/" target="blank">
+            <li className="border-2 border-slate-100 p-6 rounded-full transition-colors duration-300 ease-in hover:bg-slate-600">
+              <FontAwesomeIcon icon={faInstagram} style={{ height: "33px" }} />
+            </li>
+          </a>{" "}
+          <a href="https://twitter.com/AntonyMerleDev" target="blank">
+            <li className="border-2 border-slate-100 p-6 rounded-full transition-colors duration-300 ease-in hover:bg-slate-600">
+              <FontAwesomeIcon icon={faTwitter} style={{ height: "33px" }} />
+            </li>
+          </a>
+          <a href="mailto:antony.merle@gmail.com" target="blank">
+            <li className="border-2 border-slate-100 p-6 rounded-full transition-colors duration-300 ease-in hover:bg-slate-600">
+              <FontAwesomeIcon icon={faEnvelope} style={{ height: "33px" }} />
+            </li>
+          </a>
+        </ul>
       </footer>
     </div>
   );
