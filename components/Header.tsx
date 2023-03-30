729ce1549906608176f549a961789a95e5ea557e
Antony Merle <antony.merle@gmail.com>
2023-03-30 23:10:56 +0200

    added padding to h1

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
       className="h-screen flex flex-col justify-start space-y-4  px-6  bg-slate-800 text-center text-slate-300 md:py-48  md:px-24 md:text-left lg:px-64"
     >
       <Graph />
-      <div className="flex flex-col justify-start space-y-12 container mx-auto z-10">
+      <div className="flex flex-col justify-start container mx-auto z-10">
         <h1
           className="bg-gradient-to-r
                from-pink-400   to-orange-300 text-transparent bg-clip-text
-             flex flex-col mt-44 space-y-4  text-xl font-bold md:text-6xl lg:text-8xl"
+             flex flex-col mt-44 space-y-4 pb-12 text-xl font-bold md:text-6xl lg:text-8xl"
         >
           <span className="text-xl text-mint">Hi, my name is </span>
           <span className="text-slate-50">Antony Merle.</span>
