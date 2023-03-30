65881692e9171de5bf39e1d4cca807135b76020e
Antony Merle <antony.merle@gmail.com>
2023-03-30 22:57:36 +0200

    work on header typography

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,20 +81,19 @@ export default function Home() {
           <h1
             className="bg-gradient-to-r
                from-pink-400   to-orange-300 text-transparent bg-clip-text
-             flex flex-col mt-44 space-y-4  text-xl font-bold  md:text-6xl"
+             flex flex-col mt-44 space-y-4 py-4 text-xl font-bold md:text-6xl lg:text-8xl"
           >
             <span className="text-xl text-mint">Hi, my name is </span>
             <span className="text-slate-50">Antony Merle.</span>
             <span>I build apps for today's Internet.</span>
           </h1>
           {/* TODO: make it readable on large screen by narrowing column */}
-          <p>
-            Full Stack Web Developer with a passion for crafting intuitive and
-            user-friendly web experiences. My main focus is developing
-            responsive and dynamic websites that engage visitors and meet
-            business requirements. With 3 years of experience and a focus on
-            continual learning, I am committed to delivering high-quality and
-            innovative solutions.
+          <p className="text-xl">
+            I am a fullstack web developer with a passion for crafting intuitive
+            and user-friendly web experiences. My main focus is developing
+            websites that engage visitors and meet business requirements. With
+            three years of experience and a focus on continual learning, I am
+            committed to delivering high-quality and maintainable solutions.
           </p>
           {/* <div className="w-full h-96 relative bg-mint">
             </div> */}
