0fa71f85b3b8b6811d61f6933ed2ae3a95a6781b
Antony Merle <antony.merle@gmail.com>
2023-04-04 10:55:21 +0200

    install & config next-translate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const nextTranslate = require("next-translate-plugin");
+
+module.exports = nextTranslate();
