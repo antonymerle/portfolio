6ea329395e3946829cbe623efd0d0ac2caf8fe67
Antony Merle <antony.merle@gmail.com>
2024-10-08 23:48:49 +0200

    Refactor MainProjects component to handle null projects and update project descriptions

--- components/MainProjects.tsx
+++ components/MainProjects.tsx
@@ -5,18 +5,24 @@ import useTranslation from "next-translate/useTranslation";
 const MainProjects = () => {
   const { t } = useTranslation("projects");
 
+  console.log("len mainProjects", mainProjects.length);
+  mainProjects.forEach((project, i) => {
+    console.log("project", i, project?.title);
+  });
   const mainProjectsJSX = mainProjects.map((project, i) => {
     return (
-      <Project
-        title={project.title}
-        description={t(`main-project${i}-desc`)}
-        projectImage={project.projectImage}
-        stack={project.stack}
-        repoURL={project.repoURL}
-        liveURL={project.liveURL}
-        justify={project.justify}
-        key={i}
-      />
+      project && (
+        <Project
+          title={project.title}
+          description={t(`main-project${mainProjects.length - i}-desc`)}
+          projectImage={project.projectImage}
+          stack={project.stack}
+          repoURL={project.repoURL}
+          liveURL={project.liveURL}
+          justify={project.justify}
+          key={i}
+        />
+      )
     );
   });
 
