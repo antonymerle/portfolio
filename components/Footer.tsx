b98e4434fe472515ef67260fef1bd572bc4a91a2
Antony Merle <antony.merle@gmail.com>
2024-10-10 22:33:20 +0200

    replace social media icons by svg paths and DRY code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,61 @@
 import Image from "next/image";
 import logoFullSize from "../public/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faCodeBranch } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithubAlt,
-  faLinkedinIn,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
 const iconClass =
   "md:border-2 md:border-slate-300 p-2 md:p-6 md:rounded-full md:transition-colors md:duration-300 md:ease-in md:hover:bg-mint md:hover:text-slate-700";
-const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-    <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-  </svg>
-);
+
+const social = [
+  {
+    name: "GitHub",
+    href: "https://github.com/antonymerle/hyproom-frontend",
+    icon: (props: React.SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "X",
+    href: "https://x.com/hyproom",
+    icon: (props: React.SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+      </svg>
+    ),
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/hyproom_workspaces/",
+    icon: (props: React.SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/antony-merle-25854042/",
+    icon: (props: React.SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 20 20" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
 
 const Footer = () => {
   return (
@@ -33,80 +74,19 @@ const Footer = () => {
           target="_blank"
           aria-label="Antony Merle's portfolio repository"
           rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faCodeBranch}
-            style={{ height: "16px" }}
-            className="pl-2 hover:text-mint"
-          />
-        </a>
+        ></a>
       </h3>
 
       <ul className="flex flex-row space-x-6 md:space-x-10">
-        <li>
-          <a
-            href="https://github.com/antonymerle"
-            target="blank"
-            rel="noopener noreferrer"
-            aria-label="Antony Merle's Github profile"
-          >
-            <div className={iconClass}>
-              <FontAwesomeIcon icon={faGithubAlt} style={{ height: "33px" }} />
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://www.linkedin.com/in/antony-merle-25854042/"
-            target="blank"
-            rel="noopener noreferrer"
-            aria-label="Antony Merle's LinkedIn profile"
-          >
-            <div className={iconClass}>
-              <FontAwesomeIcon icon={faLinkedinIn} style={{ height: "33px" }} />
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://www.instagram.com/antonymerle/"
-            target="blank"
-            rel="noopener noreferrer"
-            aria-label="Antony Merle's Instagram profile"
-          >
-            <div className={iconClass}>
-              <FontAwesomeIcon icon={faInstagram} style={{ height: "33px" }} />
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://twitter.com/AntonyMerleDev"
-            target="blank"
-            rel="noopener noreferrer"
-            aria-label="Antony Merle's Twitter profile"
-          >
-            <div className={iconClass}>
-              <XIcon className="h-8 w-8" aria-hidden="true" />
-            </div>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="mailto:antony.merle@gmail.com"
-            target="blank"
-            rel="noopener noreferrer"
-            aria-label="Send an email to Antony Merle"
-          >
-            <div className={iconClass}>
-              <FontAwesomeIcon icon={faEnvelope} style={{ height: "33px" }} />
-            </div>
-          </a>
-        </li>
+        {social.map((item, i) => (
+          <li key={i}>
+            <a href={item.href} target="blank" aria-label={item.name}>
+              <div className={iconClass}>
+                <item.icon className="h-8 w-8" aria-hidden="true" />
+              </div>
+            </a>
+          </li>
+        ))}
       </ul>
     </footer>
   );
